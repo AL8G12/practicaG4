d6def5f7753685ff637d613c98d8a1bed7b94220
Esmeralda Morales Castillo <ada.nena12love@gmail.com>
2014-08-04 09:45:17 -0500

    sonido
    aplicacion de sonidos

---
+++ js/acciones.js
@@ -0,0 +1,10 @@
+//JavaScript
+	
+$(document).ready(function(e){
+$('#Beep').click(function(e){
+	alert('le acabas de dar click a Beep');
+});//click Beep
+$('#Vibrar').click(function(e){
+	alert('le acabas de dar click a Vibrar');
+});//click Vibrar
+});//ready
