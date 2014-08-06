bad60515df4b86b1dfbfb46870815bd73ca365d3
Esmeralda Morales Castillo <ada.nena12love@gmail.com>
2014-08-06 08:22:31 -0500

    sonidos
    juego de sonidos

--- js/acciones.js
+++ js/acciones.js
@@ -1,10 +1,23 @@
 //JavaScript
-	
+	var audio;
+
 $(document).ready(function(e){
-$('#Beep').click(function(e){
-	alert('le acabas de dar click a Beep');
-});//click Beep
-$('#Vibrar').click(function(e){
-	alert('le acabas de dar click a Vibrar');
-});//click Vibrar
+	document.addEventListener("deviceready",function(e){
+	
+	audio=window.plugins.LowLatencyAudio;
+	audio.preloadFX('mario','audio/mario.mp3',function(msg){},function(msg){alert('error: '+msg);})
+	
+	
+	$('#Beep').bind("tap",function(e){
+	navigator.notification.beep(1);
+});
+
+$('#Vibrar').bind("tap",function(e){
+	navigator.notification.vibrate(1000);
+});
+
+$('#Play').bind("tap",function(e){
+	audio.play('mario')
+});
+});
 });//ready
